9caf09731812b548262ad914102dab232c8dd657
pratiksha-dse <b20165@students.iitmandi.ac.in>
2022-12-18 16:27:43 +0530

    chat-loader added

--- client/src/components/chatWindow/channelBody.js
+++ client/src/components/chatWindow/channelBody.js
@@ -5,6 +5,7 @@ import { ChannelHeader, MessageInput, MessageList } from "stream-chat-react";
 const Container = styled.div`
     width: 100%;
     height: 82vh;
+    
 
     .str-chat-header-livestream {
         width: 100%;
@@ -13,6 +14,11 @@ const Container = styled.div`
 
     .str-chat__list {
         height: calc(100vh - 70px);
+        background-color:rgba(0, 0, 0, 0.4);
+        // background-image: url(https://e0.pxfuel.com/wallpapers/518/286/desktop-wallpaper-3d-abstract-colorful-doodle-drawing-small-retro-art.jpg);
+        background-image: url(https://wallpaperaccess.com/full/5494980.jpg);
+        background-size: cover;
+        // opacity:0.5;
     }
 
     .str-chat__input-flat-wrapper {
--- client/src/demos/ChatWindow.js
+++ client/src/demos/ChatWindow.js
@@ -1,12 +1,12 @@
-import React, { useState,useContext, useEffect } from "react";
-import {useHistory} from "react-router-dom"
+import React, { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom"
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
 
 import tw from "twin.macro";
 import Footer from "components/footers/MiniCenteredFooter.js";
-import {StreamChat} from 'stream-chat';
-import {Chat, Channel, Window, ChannelHeader, MessageInput, MessageList, ChannelList} from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, Window, ChannelHeader, MessageInput, MessageList, ChannelList } from 'stream-chat-react';
 // import './App.css';
 import CustomChannelList from '../components/chatWindow/customChannelList';
 import ChannelBody from '../components/chatWindow/channelBody';
@@ -15,7 +15,7 @@ import 'stream-chat-react/dist/css/v2/index.css';
 import styled from 'styled-components';
 import { AuthContext } from "../Context/AuthContext";
 import img from "../images/globe.png";
-
+import Loader from "react-js-loader";
 const Subheading = tw.span`uppercase tracking-wider text-sm`;
 
 const Container = styled.div`
@@ -32,25 +32,25 @@ const API_KEY = "v246ajm6p5d5";
 
 function ChatWindow() {
 
-    const {
-        user,
-        setUser,
-        isAuthenticated,
-        setIsAuthenticated,
-        isAdmin,
-        setIsAdmin,
-    } = useContext(AuthContext);
-    const navigate = useHistory();
+  const {
+    user,
+    setUser,
+    isAuthenticated,
+    setIsAuthenticated,
+    isAdmin,
+    setIsAdmin,
+  } = useContext(AuthContext);
+  const navigate = useHistory();
 
   const [chatClient, setChatClient] = useState(null);
   const [channel, setChannel] = useState(null);
 
   const [addingTeamChannel, setAddingTeamChannel] = useState(false);
 
-  function getHash(input){
+  function getHash(input) {
     var hash = 0, len = input.length;
     for (var i = 0; i < len; i++) {
-      hash  = ((hash << 5) - hash) + input.charCodeAt(i);
+      hash = ((hash << 5) - hash) + input.charCodeAt(i);
       hash |= 0; // to 32bit integer
     }
     return hash.toString();
@@ -84,18 +84,18 @@ function ChatWindow() {
     initChat();
 
     return () => {
-      if(chatClient) {chatClient.disconnectUser();}
+      if (chatClient) { chatClient.disconnectUser(); }
     };
 
   }, []);
 
-  if(!chatClient || !channel) {
+  if (!chatClient || !channel) {
     return (
       <>
-        <div>
-            <p>Loading Chat.....</p>
-        </div>
+        <div style={{height:"40vh"}}><br></br></div>
+        <Loader  type="spinner-cub" bgColor={"#402596"} title={"Loading Chats"} color={"#402596"} size={100} />
       </>
+
     );
   }
 
@@ -103,19 +103,19 @@ function ChatWindow() {
     <div>
       <Chat client={chatClient} theme={'messaging light'}>
         <Container>
-            <div className='left-column'>
-              <CustomChannelList onClickAdd={() => setAddingTeamChannel(true)} onClickExit={()=>{
-                if(chatClient){
-                    chatClient.disconnectUser();
-                }
-                navigate.push('/');
-              }}/>
-            </div>
-            <div className='right-column'>
-                <Channel>
-                  {addingTeamChannel? <AddingChannel onClose={() => setAddingTeamChannel(false)}/> : <ChannelBody/>}
-                </Channel>
-            </div>
+          <div className='left-column'>
+            <CustomChannelList onClickAdd={() => setAddingTeamChannel(true)} onClickExit={() => {
+              if (chatClient) {
+                chatClient.disconnectUser();
+              }
+              navigate.push('/');
+            }} />
+          </div>
+          <div className='right-column'>
+            <Channel>
+              {addingTeamChannel ? <AddingChannel onClose={() => setAddingTeamChannel(false)} /> : <ChannelBody />}
+            </Channel>
+          </div>
         </Container>
       </Chat>
     </div>
